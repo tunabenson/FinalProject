d2451261ebecf3e550489fb30435481cdecfba7f
tunabenson <habracadabra2006@gmail.com>
2025-03-20 20:20:36 -0700

    updates to structure, added some new classes, please get some work done guys

--- src/Order.java
+++ src/Order.java
@@ -1,6 +1,62 @@
+import structs.LinkedList;
+
 public class Order {
+
+    public enum ShippingType {
+        STANDARD,
+        PRIORITY,
+        OVERNIGHT
+    }
     private static int order_seed = 1000000;
-    private Product product;
-    private int quantity;
+    private LinkedList<Product> orderContent;
     private int orderId;
+
+    private ShippingType shippingType;
+    private int priority;
+
+
+    public Order(){
+        this.orderId = ++orderId;
+        this.orderContent = new LinkedList<>();
+        this.shippingType = null;
+    }
+
+    public Order(ShippingType shippingType, Product... products) {
+        this.orderContent = new LinkedList<>(products);
+        orderId = ++order_seed;
+        this.shippingType = shippingType;
+    }
+
+    public boolean isEmpty(){
+        return orderContent.isEmpty();
+    }
+
+    public void addToOrder(Product product){
+        this.orderContent.addLast(product);
+    }
+
+    public void setShippingType(int shippingType){
+
+    }
+
+
+    @Override
+    public String toString() {
+        StringBuilder str = new StringBuilder();
+        str.append("Order of: \n").append(orderContent.numberedListString());
+        str.append("\n");
+        str.append("Using ").append(getShippingType()).append("\n");
+        return str.toString();
+    }
+
+    public String getShippingType(){
+        switch (shippingType){
+            case STANDARD: return "Standard Shipping";
+            case PRIORITY: return "Rush Shipping";
+            case OVERNIGHT: return "Overnight Shipping";
+        }
+        return null; //should not happen
+    }
+
+
 }
---
+++ src/structs/PriorityQueue.java
@@ -0,0 +1,7 @@
+package structs;
+
+public class PriorityQueue {
+
+    //todo: implement this using Heap of Orders or use generics (pretty specific for Orders though) so it doenst matter
+    //implement and make sure it works
+}
